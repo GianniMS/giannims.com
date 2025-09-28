51e708f2a632d64cfd3380bbe22f382dd7c39655
giannims <giannims2002@gmail.com>
2025-09-29 01:05:38 +0200

    Minor setback fuck it we ball

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
-import BackgroundBase from '@/components/home/BackgroundBase';
-import BackgroundLayer from '@/components/home/BackgroundLayer';
-
 export default function HomePage() {
   return (
     <main className="relative w-full h-screen overflow-hidden">
-      <BackgroundBase
-        src="/images/chi-base.JPG"
-        alt="Chicago background"
-      />
-      <BackgroundLayer
-        src="/images/chi-layer.png"
-        alt="Chicago cutout overlay"
-      />
+      <h1>IN PROGRESS</h1>
     </main>
   );
 }
